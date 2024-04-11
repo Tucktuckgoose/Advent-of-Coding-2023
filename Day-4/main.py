e11747e50ab3fede148305032e8288a424f96896
Tucktuckgoose <wilsoncandler87@gmail.com>
2024-04-10 22:50:45 -0600

    Solved day 4 part 2

--- Day-4/main.py
+++ Day-4/main.py
@@ -22,6 +22,43 @@ def part1():
 
     print(totalPoints)
 
+def part2():
+    totalCards = 0
+    cardCopiesDict = {}
+    winnersDict = {}
+    mineDict = {}
+
+    with open('input.txt') as file:
+        i = 1
+        for line in file:
+            cardCopiesDict[i] = 1
+            shortenedLine = line[10:] # Get rid of the card information
+            winnersLong, mineLong = shortenedLine.split("|")
+            winners = winnersLong.split()
+            mine = mineLong.split()
+            winnersDict[i] = winners
+            mineDict[i] = mine
+            i += 1
+    
+    for i in range(1,len(cardCopiesDict)+1):
+        matches = 0
+        for aNum in mineDict[i]:
+            for aWinner in winnersDict[i]:
+                if (aNum == aWinner):
+                    #print(aNum + " matches " + aWinner)
+                    matches += 1
+                #else:
+                    #print(aNum + " does not match " + aWinner)
+        #print("Card " + str(i) + " had " + str(matches) + " matches on the card.")
+        for j in range(1, matches + 1):
+            #numCopies = cardCopiesDict[i + j]
+            #numCopies += 1
+            cardCopiesDict[i + j] += cardCopiesDict[i]
+        totalCards += cardCopiesDict[i]
+
+    print(totalCards)
+    #print(cardCopiesDict)
+
 if __name__ == "__main__":
-    part1()
-    #part2()
+    #part1()
+    part2()
